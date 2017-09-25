2b0a8d53a24b5c77fed430fd4efbc7beafd08930
Stefan Kaesche <stefan.kaesche@artcom-venture.de>
2017-09-25 12:46:12 +0200

    Missing version number increase :/

--- image-sizes.php
+++ image-sizes.php
@@ -4,7 +4,7 @@
  * Plugin Name: Image Sizes
  * Plugin URI: https://github.com/artcomventure/wordpress-plugin-cropImageSizes
  * Description: Edit all available image sizes.
- * Version: 1.3.0
+ * Version: 1.3.1
  * Text Domain: image-sizes
  * Author: artcom venture GmbH
  * Author URI: http://www.artcom-venture.de/
